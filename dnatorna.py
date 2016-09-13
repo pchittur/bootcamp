b1903d9eb370afc4588c27664f72cb449b081bf0
Priya Chittur <pchittur@caltech.edu>
2016-09-12 19:06:23 -0700

    No changes

--- dnatorna.py
+++ dnatorna.py
@@ -1,6 +1,6 @@
 """Convert DNA to RNA"""
 
 def rna(seq):
-    """Convert DNA sequence to RNA"""
+    """Convert DNA sequence to RNA. The returned string is all upper case."""
     seq = seq.upper()
     return seq.replace('T', 'U')
